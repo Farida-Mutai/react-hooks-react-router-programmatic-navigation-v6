ac69d3ed9995dc020a7c67882ef0558cd58970a1
Farida Mutai <faridamutai52@gmail.com>
2024-08-08 16:40:46 +0300

    kumalo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import { NavLink} from "react-router-dom";
-import "./NavBar.css";
-
-function NavBar() {
+import "./NavBar.css"
+function NavBar({ logout }) {
 
   return (
     <nav>
@@ -16,9 +15,11 @@ function NavBar() {
         className="nav-link"
       >
         About
-      </NavLink>
+        </NavLink>
+      {/* Add the logout function to handle the onClick event */}
+      <button onClick={logout}>Logout</button>
     </nav>
   );
-}
+};
 
 export default NavBar;
